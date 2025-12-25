178da9cde30987a34fe39bf0d813709f0da4950a
sjstein <s.joshua.stein@gmail.com>
2025-12-24 19:57:53 -0600

    first widespread release

--- version.py
+++ version.py
@@ -3,5 +3,5 @@ Version information for Run8 Industry Tool
 This file is automatically updated during the build process
 """
 
-VERSION = "1.0.6"
-BUILD_DATE = "2025-12-24 08:27"
+VERSION = "1.1.0"
+BUILD_DATE = "2025-12-24 19:46"
